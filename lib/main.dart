85f8cf9f44b1015feb1a98bd72fdd39362240c78
Lenovo <bhargavpatel6832@gmail.com>
2023-03-01 12:50:06 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:colour_palate/Screen/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Colour(
+
+      ),
+    )
+  );
+}
+
